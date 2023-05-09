7073d2e81e92abd64177460bedb8d674ec54a475
ChaeW00 <limchaewoo4819@gmail.com>
2023-05-09 18:17:17 +0900

    230509 생성 및 설정 변경

---
+++ finalkh11/src/main/java/com/kh/finalkh11/Finalkh11Application.java
@@ -0,0 +1,13 @@
+package com.kh.finalkh11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Finalkh11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Finalkh11Application.class, args);
+	}
+
+}
---
+++ finalkh11/src/test/java/com/kh/finalkh11/Finalkh11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.finalkh11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Finalkh11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
