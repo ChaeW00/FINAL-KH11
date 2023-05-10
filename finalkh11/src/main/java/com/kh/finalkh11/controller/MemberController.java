0bac7830f2a767096d2a9117c6944eeb7e70d45f
parkjieun87 <dksxlzhd0807@naver.com>
2023-05-10 10:33:06 +0900

    커밋

---
+++ finalkh11/src/main/java/com/kh/finalkh11/controller/MemberController.java
@@ -0,0 +1,15 @@
+package com.kh.finalkh11.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+	@GetMapping("/")
+	public String login() {
+		return "member/login";
+	}
+}
