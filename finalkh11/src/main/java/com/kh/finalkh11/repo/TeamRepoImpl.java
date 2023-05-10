41337eef0824c72367bf5490afdf7c5d23942dbb
ChaeW00 <limchaewoo4819@gmail.com>
2023-05-10 15:50:40 +0900

    230510 16:50

---
+++ finalkh11/src/main/java/com/kh/finalkh11/repo/TeamRepoImpl.java
@@ -0,0 +1,50 @@
+package com.kh.finalkh11.repo;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.finalkh11.dto.TeamDto;
+
+@Repository
+public class TeamRepoImpl implements TeamRepo{
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Override
+	public void insert(TeamDto teamDto) {
+		sqlSession.insert("team.add", teamDto);
+		
+	}
+
+	@Override
+	public List<TeamDto> selectList() {
+		return sqlSession.selectList("team.selectList");
+	}
+
+	@Override
+	public List<TeamDto> selecList(String column, String keyword) {
+		return null;
+	}
+
+	@Override
+	public TeamDto selectOne(int teamNo) {
+		return sqlSession.selectOne("team.selectOne", teamNo);
+	}
+
+	@Override
+	public boolean update(TeamDto teamDto) {
+		int result = sqlSession.update("team.edit", teamDto);
+		return result > 0;
+	}
+	@Override
+	public boolean delete(int teamNo) {
+		int result = sqlSession.delete("team.remove", teamNo);
+		return result > 0;
+	}
+
+
+}
