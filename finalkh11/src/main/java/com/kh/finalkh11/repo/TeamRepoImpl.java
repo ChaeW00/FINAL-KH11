0960bef7e7b00309e274af795fdb1bf1634166d4
ChaeW00 <limchaewoo4819@gmail.com>
2023-05-11 18:07:34 +0900

    230511 18:07..

--- finalkh11/src/main/java/com/kh/finalkh11/repo/TeamRepoImpl.java
+++ finalkh11/src/main/java/com/kh/finalkh11/repo/TeamRepoImpl.java
@@ -8,37 +8,49 @@ import org.springframework.stereotype.Repository;
 
 import com.kh.finalkh11.dto.TeamDto;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Repository
 public class TeamRepoImpl implements TeamRepo{
 	
 	@Autowired
 	private SqlSession sqlSession;
 	
-	@Override
+	@Override //팀 생성
 	public void insert(TeamDto teamDto) {
 		sqlSession.insert("team.add", teamDto);
 		
 	}
-
-	@Override
-	public List<TeamDto> selectList() {
+ 
+	@Override //팀 리스트
+	public List<TeamDto> selectList() { 
 		return sqlSession.selectList("team.selectList");
 	}
 
-	@Override
+	@Override //팀 상세
 	public TeamDto selectOne(int teamNo) {
 		return sqlSession.selectOne("team.selectOne", teamNo);
 	}
 
-	@Override
+	@Override // 팀 정보수정
 	public boolean update(TeamDto teamDto) {
 		int result = sqlSession.update("team.edit", teamDto);
 		return result > 0;
 	}
-	@Override
+	@Override // 팀 삭제
 	public boolean delete(int teamNo) {
 		int result = sqlSession.delete("team.remove", teamNo);
 		return result > 0;
+	}
+	@Override  //전적기록
+	public void record(TeamDto teamdto) {
+		sqlSession.update("team.record", teamdto);
+	}
+
+	@Override //팀 일정 업데이트
+	public void schedule(TeamDto teamdto) {
+		sqlSession.update("team.schedule", teamdto);
 	}
 
 
