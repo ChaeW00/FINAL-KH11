de797c4e9a8253f96692f669b165f24ecc3dce44
tewi0314 <tewi0314@gmail.com>
2023-05-12 13:13:09 +0900

    Merge remote-tracking branch 'origin/main' into junwon

--- finalkh11/src/main/java/com/kh/finalkh11/controller/MatchController.java
+++ finalkh11/src/main/java/com/kh/finalkh11/controller/MatchController.java
@@ -8,16 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.kh.finalkh11.Repo.MatchRepo;
 import com.kh.finalkh11.dto.MatchDto;
+import com.kh.finalkh11.repo.MatchRepo;
 
 @Controller
 public class MatchController {
