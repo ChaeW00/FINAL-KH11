d311718ea23f218419af244d7bb06e41da1bf184
ChaeW00 <limchaewoo4819@gmail.com>
2023-05-31 14:49:21 +0900

    멤버리스트 구현 중

--- finalkh11/src/main/java/com/kh/finalkh11/controller/TeamInController.java
+++ finalkh11/src/main/java/com/kh/finalkh11/controller/TeamInController.java
@@ -21,38 +21,36 @@ import com.kh.finalkh11.repo.TeamRepo;
 @Controller
 @RequestMapping("/team_in")
 public class TeamInController {
-	
+
 	private final TeamRepo teamRepo;
 	private final TeamMemberRepo teamMemberRepo;
-	
+
 	@Autowired
 	public TeamInController(TeamRepo teamRepo, TeamMemberRepo teamMemberRepo) {
 		this.teamRepo = teamRepo;
 		this.teamMemberRepo = teamMemberRepo;
 	}
 
-	@GetMapping("/member")
-	public String memberList(@RequestParam int teamNo, Model model, HttpServletRequest request) {
-	    // 팀 멤버 리스트 조회
-	    TeamDto teamDto = teamRepo.selectOne(teamNo); // teamNo에 해당하는 팀 정보 조회
-	    List<TeamMemberDto> teamMemberList = teamMemberRepo.getTeamMemberList(teamNo); // teamNo에 해당하는 팀 멤버 리스트 조회
+	@GetMapping("/member/{teamNo}")
+	public String memberList(@PathVariable int teamNo, Model model, HttpSession session) {
+		// 현재 로그인한 사용자의 정보를 가져옴
+		String memberId = (String) session.getAttribute("memberId"); // 세션에서 로그인한 사용자의 ID를 가져옴
 
-	    // 현재 로그인한 사용자의 정보를 가져옴
-	    HttpSession session = request.getSession();
-	    String currentUserId = (String) session.getAttribute("currentUserId"); // 세션에서 로그인한 사용자의 ID를 가져옴
+		// 현재 로그인한 사용자가 해당 팀에 가입한 사용자인지 체크
+		boolean isTeamMember = teamMemberRepo.checkIfTeamMember(memberId, teamNo);
 
-	    // 현재 로그인한 사용자가 해당 팀에 가입한 사용자인지 체크
-	    boolean isTeamMember = teamMemberRepo.checkIfTeamMember(currentUserId, teamNo);
-	    System.out.println(isTeamMember);
+		// 접근 권한이 없는 경우에는 에러 페이지로 이동하도록 처리
+//		if (!isTeamMember) {
+//			return "error"; // 접근 권한이 없을 때 error.jsp로 이동
+//		}
 
-//	    // 접근 권한이 없는 경우에는 에러 페이지로 이동하도록 처리
-//	    if (!isTeamMember) {
-//	        return "error"; // 접근 권한이 없을 때 error.jsp로 이동
-//	    }
+		// 팀 멤버 리스트 조회
+		TeamDto teamDto = teamRepo.selectOne(teamNo); // teamNo에 해당하는 팀 정보 조회
+		List<TeamMemberDto> teamMemberList = teamMemberRepo.getTeamMemberList(teamNo); // teamNo에 해당하는 팀 멤버 리스트 조회
 
-	    model.addAttribute("teamVO", teamDto);
-	    model.addAttribute("teamMemberList", teamMemberList);
+		model.addAttribute("teamVO", teamDto);
+		model.addAttribute("teamMemberList", teamMemberList);
 
-	    return "team_in/member";
+		return "team_in/member";
 	}
 }
