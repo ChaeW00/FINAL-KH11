b16f468186d9908f8f1c420a86cf264d95792935
tewi0314 <tewi0314@gmail.com>
2023-06-09 17:22:03 +0900

    6/9

--- finalkh11/src/main/java/com/kh/finalkh11/controller/TeamController.java
+++ finalkh11/src/main/java/com/kh/finalkh11/controller/TeamController.java
@@ -192,5 +192,13 @@ public class TeamController {
 //	public String myTeamFail() {
 //		return "team/myTeamFail";
 //	}
+    
+  @GetMapping("/recruit-member")
+  public String recruit(Model model) {
+	  List<TeamDto> list = teamRepo.selectList();
+	  model.addAttribute("TeamList", list);
+	  return "team/recruit-member";
+  }
+  
 
 }
